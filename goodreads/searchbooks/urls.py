61af7ed607adb489fc7d15a876546e0f4b11cf0b
Laure Runser <laure-runser@tutanota.com>
2022-03-22 15:39:08 +0100

    add book details view

--- goodreads/searchbooks/urls.py
+++ goodreads/searchbooks/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+appname = 'searchbooks'
 urlpatterns = [
     path('', views.index, name='index'),
+
+    path('<int:book_id>/', views.book_detail, name='book_detail')
 ]
--- goodreads/searchbooks/views.py
+++ goodreads/searchbooks/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import Book
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the index.")
+
+
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
+    return render(request, 'search/book_detail.html', {'book': book})
