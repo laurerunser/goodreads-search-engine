7fd3f3af369e823ce10406e71d42b031ace4f8b7
Laure Runser <laure-runser@tutanota.com>
2022-03-29 14:53:27 +0200

    add tfidf + cosine similarity to do book recommendations

--- goodreads/searchbooks/urls.py
+++ goodreads/searchbooks/urls.py
@@ -5,6 +5,5 @@ from . import views
 appname = 'searchbooks'
 urlpatterns = [
     path('', views.index, name='index'),
-
     path('<int:book_id>/', views.book_detail, name='book_detail')
 ]
--- goodreads/searchbooks/views.py
+++ goodreads/searchbooks/views.py
@@ -1,15 +1,46 @@
 from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
 from django.http import HttpResponse
+from django.template import Context
 
 from .models import Book
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    books = Book.objects.all()
+    return render(request, 'search/index.html', {'books': books},)
 
 
 def book_detail(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
-    return render(request, 'search/book_detail.html', {'book': book})
+
+    all_books = Book.objects.all()
+
+    # get all the books descriptions
+    all_descriptions = []
+    for data in all_books:
+        all_descriptions.append(data.description)
+
+    # apply TFIDF on books
+    vectorizer = TfidfVectorizer()
+    matrix_tfidf = vectorizer.fit_transform(all_descriptions)
+
+    # prep the reference book
+    query_tfidf = vectorizer.transform([book.description])
+
+    # # get the cosine distance and then keep the 20 best
+    distances = cosine_similarity(query_tfidf, matrix_tfidf).flatten()
+    best = np.argsort(distances)
+
+    # get the books that correspond to the index we found
+    recs = []
+    for a in best:
+        recs = [Book.objects.get(pk=a)] + recs
+
+    # return the request
+    c = {'book': book, 'recs': recs}
+    return render(request, 'search/book_detail.html', c)
+
