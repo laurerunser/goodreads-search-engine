8ae4ddd84cb8710214c806fab841f1b9e1f3340d
OLIVIER lily <lily.univ@gmail.com>
2022-05-22 12:40:27 +0200

    bug bizarre

--- goodreads/searchbooks/import.py
+++ goodreads/searchbooks/import.py
@@ -1,13 +1,22 @@
 import csv
-from .models import Book # imports the model
-with open('searchbooks/data/test.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        p = Book(id=row['id'], title=row['title'], description=row['description'], 
-        link=row['link'], series=row['series'], cover_link=row['cover_link'], 
-        author_name=row['author'], author_link=row['author_link'], date_published=row['date_published'], 
-        average_rating=row['average_rating'], ratings_counts=row['rating_count'], 
-        ratings_5_stars=row['five_star_ratings'], ratings_4_stars=row['four_star_ratings'], 
-        ratings_3_stars=row['three_star_ratings'], ratings_2_stars=row['two_star_ratings'], 
-        ratings_1_stars=row['one_star_ratings'], review_count=row['review_count'] )
-        p.save()
+from searchbooks.models import Book # imports the model
+
+if __name__ == '__main__':
+    main()
+
+def main():
+    with open('searchbooks/data/test.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            p = Book(id=row['id'], title=row['title'], description=row['description'], 
+            link=row['link'], series=row['series'], cover_link=row['cover_link'], 
+            author_name=row['author'], author_link=row['author_link'], date_published=row['date_published'], 
+            average_rating=row['average_rating'], ratings_counts=row['rating_count'], 
+            ratings_5_stars=row['five_star_ratings'], ratings_4_stars=row['four_star_ratings'], 
+            ratings_3_stars=row['three_star_ratings'], ratings_2_stars=row['two_star_ratings'], 
+            ratings_1_stars=row['one_star_ratings'], review_count=row['review_count'] )
+            p.save()
+
+
+
+        
